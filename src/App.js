639297a06d424aeba03ca60ab1926848b2111212
Ravi Nimrani <137795244+RaviNimrani@users.noreply.github.com>
2024-06-27 19:32:33 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { ToastContainer } from "react-toastify";
+import Form from "./Components/Form/Form";
+
+import MovieList from "./Components/MovieList/MovieList";
+import Navbar from "./Components/Navbar/Navbar";
+import "./app.css";
+import "react-toastify/dist/ReactToastify.css";
+import EditContextProvider from "./editContext";
+import MovieDetails from "./Components/MovieDetails/MovieDetails";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./Components/HomePage/HomePage";
+
+function App() {
+  const router = createBrowserRouter([
+    { path: "/", element: <HomePage /> },
+    { path: "/:id", element: <MovieDetails /> },
+  ]);
+  return (
+    <>
+      <ToastContainer />
+      <RouterProvider router={router} />
+      {/* <MovieDetails /> */}
+    </>
+  );
+}
+
+export default App;
